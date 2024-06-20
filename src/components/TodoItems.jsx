f7cc58ad257800ce097d05ed1435bbface25f96c
sumit878 <sumit9794964787@gmail.com>
2024-06-21 02:32:42 +0530

    Todo-List

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+import not_tick from '../assets/not_tick.png'
+import delete_icon from '../assets/delete.png'
+
+const TodoItems= ({text,id,isComplete,deleteTodo,toggle}) => {
+  return(
+    <div className='flex items-center my-3 gap-3'>
+     <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+       <img className='w-7' src={isComplete ? tick : not_tick} alt="Error"/>
+       <p className={`tex-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
+     </div>
+     <img onClick={()=>{deleteTodo(id)}} src={delete_icon} alt="Error" className='w-3.5 cursor-pointer'/>
+    </div>
+    )
+}
+
+export default TodoItems;
